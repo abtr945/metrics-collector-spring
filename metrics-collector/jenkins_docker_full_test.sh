88fea0f796e5a4c45d09bd78ebabe59e148c997d
An Binh Tran <AnBinh.Tran@nicta.com.au>
2014-09-24 14:11:36 +1000

    fix jenkins script ;

--- metrics-collector/jenkins_docker_full_test.sh
+++ metrics-collector/jenkins_docker_full_test.sh
@@ -4,7 +4,7 @@
 docker build -t webapp ${WORKSPACE}/metrics-collector
 
 # Spawn a Docker container from the Image <webapp> and run the short_test_suite
-docker run --name ${JOB_NAME} webapp sh -c '/var/lib/tomcat7/bin/startup.sh && sleep 20 && /usr/bin/ruby /project/dockertests/short_test_suite.rb && /usr/bin/ruby /project/dockertests/long_test_suite.rb'
+docker run --name ${JOB_NAME} webapp sh -c '/var/lib/tomcat7/bin/startup.sh && sleep 20 && /usr/bin/ruby /project/dockertests/short_test_suite.rb ; /usr/bin/ruby /project/dockertests/long_test_suite.rb'
 
 # Remove the Docker container after test
 docker rm ${JOB_NAME}

7431f10da15297b09d663959bfac7664e261e40c
An Binh Tran <an.tran@unswalumni.com>
2014-10-10 11:21:50 +1100

    Update jenkins_docker_commit_test.sh

--- metrics-collector/jenkins_docker_commit_test.sh
+++ metrics-collector/jenkins_docker_commit_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the Docker Image (webapp) from the Dockerfile
-docker build -t webapp ${WORKSPACE}/metrics-collector
+docker build -t ${JOB_NAME}-commit ${WORKSPACE}/metrics-collector
 
 # Spawn a Docker container from the Image <webapp> and run the short_test_suite
-docker run --name ${JOB_NAME} webapp sh -c '/var/lib/tomcat7/bin/startup.sh && sleep 20 && /usr/bin/ruby /project/dockertests/short_test_suite.rb'
+docker run --name ${JOB_NAME} ${JOB_NAME}-commit sh -c '/var/lib/tomcat7/bin/startup.sh && sleep 20 && /usr/bin/ruby /project/dockertests/short_test_suite.rb'
 
 # Remove the Docker container after test
 docker rm ${JOB_NAME}

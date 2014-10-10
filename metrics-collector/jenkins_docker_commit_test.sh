015934640d8ee3033d4f161d9f6e8ff4ab710ed0
An Binh Tran <AnBinh.Tran@nicta.com.au>
2014-10-10 13:31:38 +1100

    fix docker setup

--- metrics-collector/jenkins_docker_commit_test.sh
+++ metrics-collector/jenkins_docker_commit_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Build the Docker Image (webapp) from the Dockerfile
+echo '[script] Build Docker Image for Commit test started...'
 docker build -t ${JOB_NAME}-commit ${WORKSPACE}/metrics-collector
+echo '[script] Build Docker Image for Commit test completed'
 
 # Spawn a Docker container from the Image <webapp> and run the short_test_suite
-docker run --name ${JOB_NAME} ${JOB_NAME}-commit sh -c '/var/lib/tomcat7/bin/startup.sh && sleep 20 && /usr/bin/ruby /project/dockertests/short_test_suite.rb'
+echo '[script] Spawn Docker container and run Commit test started...'
+docker run --name ${JOB_NAME} ${JOB_NAME}-commit sh -c '/opt/tomcat7/bin/startup.sh && sleep 20 && /usr/bin/ruby /project/dockertests/short_test_suite.rb'
+echo '[script] Spawn Docker container and run Commit test completed'
 
 # Remove the Docker container after test
+echo '[script] Clean up Docker container started...'
 docker rm ${JOB_NAME}
+echo '[script] Clean up Docker container completed'

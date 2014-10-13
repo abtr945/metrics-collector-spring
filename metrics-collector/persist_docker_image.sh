864f2dee348eb89fa6efb3a36406b7fb66421f38
An Binh Tran <an.tran@unswalumni.com>
2014-10-13 14:33:42 +1100

    Update persist_docker_image.sh

--- metrics-collector/persist_docker_image.sh
+++ metrics-collector/persist_docker_image.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Persisted Docker Image name with the following format: <registry_location>/<jenkins_job_name>:<jenkins_build_number>
+# <registry_location> can be: Docker public image registry, or private Docker registry (<registry_host>:<registry_port>)
+PERSISTED_IMAGE_NAME="54.66.148.247:5000/${JOB_NAME}:${BUILD_NUMBER}"
+
 # Tag the current periodic build image with the build number
 echo '[script] Tag the image with the periodic build number started...'
 docker tag ${JOB_NAME} ${PERSISTED_IMAGE_NAME}

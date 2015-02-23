d66851341a7f6b75332f1f40856c781af88307ea
An Binh Tran <an.tran@unswalumni.com>
2015-02-23 19:36:21 +1100

    Update test_spec.rb

--- metrics-collector/test_spec.rb
+++ metrics-collector/test_spec.rb
@@ -1,15 +1,12 @@
 describe "Docker Image" do
-
   before(:all) do
     @image = Docker::Image.all.detect{
       |image| image.info["Repository"] == "abtran/metrics-collector:10"
     }
   end
-
   it "should exist" do
     expect(@image).not_to be_nil
   end
-
   it "should expose port 8080" do
     expect(@image.json["config"]["ExposedPorts"].has_key?("8080")).to be_true
   end

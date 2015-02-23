a0df1863e795812331052b5bdfd35e0211e5283c
An Binh Tran <an.tran@unswalumni.com>
2015-02-23 19:34:08 +1100

    Update and rename metrics-collector to metrics-collector/test_spec.rb

---
+++ metrics-collector/test_spec.rb
@@ -0,0 +1,17 @@
+describe "Docker Image" do
+
+  before(:all) do
+    @image = Docker::Image.all.detect{
+      |image| image.info["Repository"] == "abtran/metrics-collector:10"
+    }
+  end
+
+  it "should exist" do
+    expect(@image).not_to be_nil
+  end
+
+  it "should expose the default port" do
+    expect(@image.json["config"]["ExposedPorts"].has_key?("22/tcp")).to be_true
+  end
+
+end  
